5833744eecbf1bd0bebde0383730340831ab911f
Joel Perca <52427932+reqhiem@users.noreply.github.com>
2022-07-17 12:15:56 -0500

    feat: 🎸 Dashboard
    Creating dashboard route & updating Main.tsx & Designing Dashboard page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react' 
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { 
+  BrowserRouter, 
+  Routes,
+  Route
+} from "react-router-dom";
+
+import App from "./App";
+import Dashboard from './routes/Dashboard/Dashboard';
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/admin/dashboard" element={<Dashboard/>} />
+          <Route path="*" element={<div>404</div>} />
+        </Routes>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
-)
+);
