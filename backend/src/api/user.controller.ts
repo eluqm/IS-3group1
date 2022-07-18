cab05eb0df6bb8b0dde653e5f38b4c54adbbfb45
MisaelVM <mvilchezmo@unsa.edu.pe>
2022-07-18 10:12:30 -0500

    refactor: 💡 Refactorizacion de User

--- backend/src/api/user.controller.ts
+++ backend/src/api/user.controller.ts
@@ -1,42 +1,40 @@
-import { RequestHandler } from 'express'
-import { toUnicode } from 'punycode'
-import User from '../models/User'
-
+import { RequestHandler } from 'express';
+import User from '../models/User';
 
 class UserController {
   static getUsers: RequestHandler = async (req, res) => {
     try {
-      const users = await User.find()
-      return res.json(users)
+      const users = await User.find();
+      return res.json(users);
     } catch (error) {
-      res.json(error)
+      return res.json(error);
     }
-  }
+  };
 
   static getUser: RequestHandler = async (req, res) => {
     try {
-      const userFound = await User.findById(req.params.id)
-      return res.json(userFound)
+      const userFound = await User.findById(req.params.id);
+      return res.json(userFound);
     } catch (error) {
-      return res.status(204).json()
+      return res.json(error);
     }
-  }
+  };
 
   static createUser: RequestHandler = async (req, res) => {
     try {
-      //verify repeated elements
+      // verify repeated elements
       const userFound = await User.findOne({
         username: req.query.username,
-        email: req.query.email  
-      })
+        email: req.query.email,
+      });
 
       if (userFound) {
         return res.status(409).json({
-          error: `The User ${req.query.username} already exists`
-        })
+          error: `The User ${req.query.username} already exists`,
+        });
       }
 
-      let {username, email, name, lastName, password, typeUser} = req.query
+      let { username, email, name, lastName, password, typeUser } = req.query;
 
       const user = new User({
         username,
@@ -44,15 +42,37 @@ class UserController {
         name,
         lastName,
         password,
-        typeUser
-      })
+        typeUser,
+      });
 
-      const savedUser = await user.save()
-      res.json(savedUser)
+      const savedUser = await user.save();
+      res.json(savedUser);
     } catch (error) {
-      res.json({error})
+      res.json({ error });
     }
-  } 
+  };
+
+  static deleteUser: RequestHandler = async (req, res) => {
+    try {
+      const userDeleted = await User.findByIdAndDelete(req.params.id);
+      return res.json(userDeleted);
+      
+    } catch (error) {
+      return res.json(error);
+    }
+  }
+
+  static updateUser: RequestHandler = async (req, res) => {
+    try {
+      const userUpdated = await User.findByIdAndUpdate(req.params.id, req.query, {
+        new: true,
+      });
+      return res.json(userUpdated);
+    } catch (error) {
+      return res.json(error);
+    }
+  }
+
 }
 
-export default UserController
+export default UserController;
