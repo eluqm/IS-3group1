79573e671898e2b632a1a3b8d93798a44a5ee89d
MisaelVM <mvilchezmo@unsa.edu.pe>
2022-07-25 12:33:52 -0500

    feat: 🎸 Controllers para CallForPaper

--- backend/src/api/evento.controller.ts
+++ backend/src/api/evento.controller.ts
@@ -1,9 +1,60 @@
-import {RequestHandler, response} from 'express';
+import { RequestHandler } from 'express';
+import CallForPaper from '../models/CallForPaper';
 
-class EventoController {
-  static getEventos : RequestHandler = async (req, res) => {
-      return res.json({msg: 'Eventos'})
-  }
+class CallForPaperController {
+  static getCallForPapers : RequestHandler = async (req, res) => {
+    try {
+      const callForPapers = await CallForPaper.find();
+      return res.status(200).json({ callForPapers });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  };
+
+  static getCallForPaper : RequestHandler = async (req, res) => {
+    try {
+      const { id: callForPaperID } = req.params;
+      const callForPaper = await CallForPaper.findOne({ _id: callForPaperID });
+      if (!callForPaper)
+        return res.status(404).json({ msg: `No Event with id: ${callForPaperID}` });
+      return res.status(200).json({ callForPaper });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  };
+
+  static createCallForPaper : RequestHandler = async (req, res) => {
+    try {
+      const callForPaper = await CallForPaper.create(req.body);
+      res.status(201).json({ callForPaper });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  };
+
+  static deleteCallForPaper : RequestHandler = async (req, res) => {
+    try {
+      const { id: callForPaperID } = req.params;
+      const callForPaper = await CallForPaper.findOneAndDelete({ _id: callForPaperID });
+      if (!callForPaper)
+        return res.status(404).json({ msg: `No Event with id: ${callForPaperID}` });
+      return res.status(200).json({ callForPaper });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  };
+
+  static updateCallForPaper : RequestHandler = async (req, res) => {
+    try {
+      const { id: callForPaperID } = req.params;
+      const callForPaper = await CallForPaper.findOneAndUpdate({ _id: callForPaperID }, req.body);
+      if (!callForPaper)
+        return res.status(404).json({ msg: `No Event with id: ${callForPaperID}` });
+      return res.status(200).json({ callForPaper });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  };
 }
 
-export default EventoController;
+export default CallForPaperController;
--- backend/src/routes/event.routes.ts
+++ backend/src/routes/event.routes.ts
@@ -1,9 +1,13 @@
 import express from 'express'
-import EventController from '../api/evento.controller'
+import CallForPaperController from '../api/evento.controller'
 
 const router = express.Router()
 
 //http://domain.com/api/v1/eventos
-router.get('/', EventController.getEventos);
+router.get('/', CallForPaperController.getCallForPapers);
+router.get('/:id', CallForPaperController.getCallForPaper);
+router.post('/', CallForPaperController.createCallForPaper);
+router.delete('/:id', CallForPaperController.deleteCallForPaper);
+router.patch('/:id', CallForPaperController.updateCallForPaper);
 
-export default router
+export default router;
